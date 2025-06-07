7796ae60dd4e29583e7f1b972d41b857d7d99e8e
Pr0pyh <andrej.drmanovic@gmail.com>
2025-06-07 17:26:55 +0200

    pucanje svrake

--- tutorial/svraka/svraka.gd
+++ tutorial/svraka/svraka.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 @export var anim_player: AnimationPlayer
+@export var audio_player: AudioStreamPlayer
+@export var shoot_dialogue: StringName
+@export var dialogue: Label
 
 
 func _process(delta: float) -> void:
@@ -18,3 +21,12 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if(anim_name == &"Talk"):
 		anim_player.stop()
 		anim_player.play(&"Idle")
+
+
+func _on_hitbox_got_hit() -> void:
+	dialogue.text = shoot_dialogue
+	audio_player.play()
+	anim_player.stop()
+	for i in 3:
+		anim_player.play(&"Talk")
+		await anim_player.animation_finished
