624502784a6db3de86abc845b2dc4e8ccd43fd11
Pr0pyh <andrej.drmanovic@gmail.com>
2025-06-07 13:10:09 +0200

    impact kaka

--- shot/impact.gd
+++ shot/impact.gd
@@ -1,6 +1,8 @@
 class_name Impact
 extends Node3D
 
+@export var anim_player: AnimationPlayer
 
 func _ready():
+	anim_player.speed_scale = 1.0/Engine.time_scale
 	get_tree().create_timer(0.2, false, false, true).timeout.connect(queue_free)
--- shot/shot.gd
+++ shot/shot.gd
@@ -4,12 +4,14 @@ extends Node3D
 
 @export var raycast: RayCast3D
 @export var impact_scene: PackedScene
+@export var anim_player: AnimationPlayer
 
 
 func setup(pos: Vector3, direction: Vector3):
 	global_position = pos
 	look_at(direction)
 	trigger()
+	anim_player.speed_scale = 1.0/Engine.time_scale
 
 
 func trigger():
